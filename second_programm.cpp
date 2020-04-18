68ca297deb2991109dc461a79e74de693700a09a
VladimirNikel <vova_niemisto@mail.ru>
2020-04-18 12:47:08 +0700

    addSemphor

--- second_programm.cpp
+++ second_programm.cpp
@@ -5,12 +5,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h> 
+#include <semaphore.h>
 
 using namespace std;
 
 string tmp = "";
+sem_t semaphore;
 
 void *content_start(void *s){
+	sem_wait(&semaphore);
 	pid_t pr;
  	pr = fork();
 	int status;
@@ -21,6 +24,8 @@ void *content_start(void *s){
 	else if (pr > 0){
 		wait(&status);
 	}
+	//sleep(2);
+	sem_post(&semaphore);
 }
 
 int main(int argc, char *argv[]){
@@ -29,30 +34,23 @@ int main(int argc, char *argv[]){
 	cout<<"Введите интересующий Вас ip-адрес: ";
 	string adres;
 	cin>>adres;
-	/*
-	string name="first";
-	string tmp="";
-	char *argm[3];
-	argm[0]=(char*) name.c_str();
-	argm[2]=NULL;
-	*/
 	for(int i=adres.size()-1;adres[i]!='.';i--)	//удаление символов до первой точки
 		adres.pop_back();
-
 	pthread_t thread[254];
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
    	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
+   	sem_init(&semaphore, 0, 1);
 	for(int i=1;i<255;i++){
 		tmp = adres+std::to_string(i);
 		rc = pthread_create(&thread[i], &attr, content_start, (void*) &tmp);
-		
 		if(rc){
  			cout<<" Error:unable to create thread, "<<rc<<endl;
          	exit(-1);
       	}
 		pthread_join(thread[i], &status);
 	}
+	sem_destroy(&semaphore);
+	pthread_attr_destroy(&attr); 
 	return 0;
 }
